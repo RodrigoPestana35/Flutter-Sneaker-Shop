ff4fa246ec7b4383a8ca846301d4421080e10f5a
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 14:21:05 +0100

    Refactor ShoeTile widget to include shoe image, description, price, and add to cart button

--- lib/pages/shoe_tile.dart
+++ lib/pages/shoe_tile.dart
@@ -12,20 +12,70 @@ class ShoeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(left: 25),
-      width: 200,
+      width: 300,
       decoration: BoxDecoration(
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(children: [
-        //shoe image
-        Image.asset(shoe.image),
-        //description
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //shoe image
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(shoe.image),
+          ),
+          //description
+          Text(
+            shoe.description,
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
 
-        //price
+          //price
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    shoe.name,
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  Text(
+                    shoe.price,
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
+              ),
 
-        //add to cart button
-      ]),
+              //plus button
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          )
+
+          //add to cart button
+        ],
+      ),
     );
   }
 }
