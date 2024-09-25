2e6b2e779bb4b7549d1464b51a077d0e1862bc30
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 14:02:35 +0100

    Refactor ShopPage widget to include search bar, message, and hot products section

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -10,8 +12,75 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Shop Page"),
+    return Column(
+      children: [
+        //search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Search",
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+        //message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Text(
+            "above those who walk... are those who fly!",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        //hot products
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "Hot Products 🔥",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "See all",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ShoeTile();
+            },
+          ),
+        )
+      ],
     );
   }
 }
