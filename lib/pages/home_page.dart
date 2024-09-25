578e906248a6d5f23e9b8b773ad0060de6b533f9
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 12:53:14 +0100

    Add CartPage and ShopPage widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sneakershop/components/bottom_nav_bar.dart';
+import 'package:sneakershop/pages/cart_page.dart';
+import 'package:sneakershop/pages/shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,10 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //variable to hold the current selected tab
+  int selectedTab = 0;
+  //navigate to the selected tab
+  void navigateBottomBar(int index) {
+    setState(() {
+      selectedTab = index;
+    });
+  }
+
+  //pages to display
+  final List<Widget> pages = [
+    //shop page
+    const ShopPage(),
+    //cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
     );
   }
 }
