5d95bb5ad2153edd45c7dd21944fcb580b51adc9
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 12:35:52 +0100

    Refactor IntroPage widget and add navigation to HomePage

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneakershop/pages/home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -11,6 +12,7 @@ class IntroPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               //logo
               Padding(
@@ -51,16 +53,26 @@ class IntroPage extends StatelessWidget {
               ),
 
               //start now button
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[900],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.all(25),
-                child: Center(
-                  child: Text(
-                    "Shop Now",
-                    style: TextStyle(color: Colors.white),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: Center(
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               )
