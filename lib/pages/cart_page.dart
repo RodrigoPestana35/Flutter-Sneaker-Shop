6ebcbcba64826d5858c298f182152fea8ed203c5
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 22:56:40 +0100

    Refactor CartItem widget to display shoe details in the cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneakershop/components/cart_item.dart';
+import 'package:sneakershop/models/cart.dart';
+import 'package:sneakershop/models/shoe.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,9 +14,41 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Cart Page",
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            // heading
+            Text(
+              "My Cart",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[800],
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getCart().length,
+                itemBuilder: (context, index) {
+                  //get individual shoe
+                  Shoe individualShoe = value.getCart()[index];
+
+                  //return the car item
+                  return CartItem(
+                    shoe: individualShoe,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
