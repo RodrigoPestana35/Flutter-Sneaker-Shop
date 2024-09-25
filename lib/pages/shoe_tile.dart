81e4d875cd8660abe32ad32d71e7874a6050662b
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 14:25:36 +0100

    Refactor ShoeTile widget to include shoe name, price, and add to cart button

--- lib/pages/shoe_tile.dart
+++ lib/pages/shoe_tile.dart
@@ -34,43 +34,52 @@ class ShoeTile extends StatelessWidget {
           ),
 
           //price
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    shoe.name,
-                    style: TextStyle(
-                      color: Colors.grey[800],
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.grey[800],
+                      ),
                     ),
-                  ),
-                  Text(
-                    shoe.price,
-                    style: TextStyle(
-                      color: Colors.grey[800],
+                    const SizedBox(
+                      height: 5,
                     ),
-                  ),
-                ],
-              ),
+                    Text(
+                      '${shoe.price}€',
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                  ],
+                ),
 
-              //plus button
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    bottomRight: Radius.circular(12),
+                //plus button
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
                   ),
                 ),
-                child: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+              ],
+            ),
           )
 
           //add to cart button
