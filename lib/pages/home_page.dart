02ce20c6e684068f342b6fc7812a7ebe2d5c9b43
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 13:49:35 +0100

    Refactor HomePage widget to include Drawer with improved styling

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,11 +43,63 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
-            icon: const Icon(Icons.menu),
+            icon: Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: const Icon(Icons.menu),
+            ),
           );
         }),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset(
+                    "lib/images/vecteezy_nike-transparent-png-nike-free-png_19956200.png",
+                    color: Colors.white,
+                  ),
+                ),
+
+                //other pages
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text(
+                      "Shop",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text(
+                      "About",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25, bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text(
+                  "Logout",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: pages[selectedTab],
     );
   }
