a487ad9d4d71d0a67ba1309f5267bb9e2ca164a1
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 22:51:50 +0100

    Refactor ShoeTile widget to include an onTap function for adding shoes to the cart

--- lib/pages/shoe_tile.dart
+++ lib/pages/shoe_tile.dart
@@ -3,9 +3,11 @@ import 'package:sneakershop/models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
   Shoe shoe;
+  void Function()? onTap;
   ShoeTile({
     super.key,
     required this.shoe,
+    required this.onTap,
   });
 
   @override
@@ -67,18 +69,21 @@ class ShoeTile extends StatelessWidget {
                 ),
 
                 //plus button
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      bottomRight: Radius.circular(12),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomRight: Radius.circular(12),
+                      ),
                     ),
-                  ),
-                  child: const Icon(
-                    Icons.add,
-                    color: Colors.white,
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
