4dacc4dcc39227b28e71de8a2f1158ff4ac4420c
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 12:44:33 +0100

    Refactor BottomNavBar widget to improve styling and add active tab border

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
@@ -7,16 +9,22 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(tabs: [
-        GButton(
-          icon: Icons.home,
-          text: 'Shop',
-        ),
-        GButton(
-          icon: Icons.shopping_cart,
-          text: 'Cart',
-        ),
-      ]),
+      child: GNav(
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+          ),
+          GButton(
+            icon: Icons.shopping_cart,
+            text: 'Cart',
+          ),
+        ],
+      ),
     );
   }
 }
