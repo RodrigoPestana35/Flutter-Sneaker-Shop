f19877d641d79a011d3aef5940d2cd61277eb91a
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 12:51:00 +0100

    Refactor BottomNavBar widget to improve styling and add active tab border

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -4,16 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({super.key});
+  void Function(int)? onTabChange;
+  BottomNavBar({
+    super.key,
+    required this.onTabChange,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         color: Colors.grey[400],
         activeColor: Colors.grey.shade700,
         tabActiveBorder: Border.all(color: Colors.white),
         tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
         tabs: [
           GButton(
             icon: Icons.home,
